4925c646c7dc243507adf656f2b8f7a5d6d96944
Mahendra Patel <Mpatel6695@conestogac.on.ca>
2024-02-15 01:14:59 -0500

    declares a public class named "Position"

--- GemHuntersGame/Program.cs
+++ GemHuntersGame/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+namespace GemHunters
+{
+    //  declares a public class named "Position"
+    public class Position
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
