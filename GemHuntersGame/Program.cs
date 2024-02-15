6edaf20f0407ae1abc68d3264e4976daab3f64e8
Mahendra Patel <Mpatel6695@conestogac.on.ca>
2024-02-15 01:14:59 -0500

    #defines a method named Move within the Player class and declares a public class named Player.

--- GemHuntersGame/Program.cs
+++ GemHuntersGame/Program.cs
@@ -1,2 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+namespace GemHunters
+{
+    //  declares a public class named "Position"
+    public class Position
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+
+    // declares a public class named Player.
+    public class Player
+    {
+
+        // declares a read-only property Name 
+
+        public string Name { get; }
+        public Position Position { get; set; }
+        public int GemCount { get; set; }
+
+        public Player(string name, Position position)
+        {
+            Name = name;
+            Position = position;
+            GemCount = 0;
+        }
+        //defines a method named Move within the Player class.
+
+        public void Move(char direction)
+        {
+            switch (direction)
+            {
+                case 'U':
+                    Position = new Position(Position.X, Position.Y - 1);
+                    break;
+                case 'D':
+                    Position = new Position(Position.X, Position.Y + 1);
+                    break;
+                case 'L':
+                    Position = new Position(Position.X - 1, Position.Y);
+                    break;
+                case 'R':
+                    Position = new Position(Position.X + 1, Position.Y);
+                    break;
+                default:
+                    Console.WriteLine("Invalid direction!");
+                    break;
+            }
+        }
+    }
+}
